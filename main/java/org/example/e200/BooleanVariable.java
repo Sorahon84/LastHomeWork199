7732b44f394ac5c0de7c50fe35ec787f7e3f8b89
Sorahon84 <shohkuch@gmail.com>
2024-09-16 17:14:37 -0500

    one added

---
+++ main/java/org/example/e200/BooleanVariable.java
@@ -0,0 +1,10 @@
+package org.example.e200;
+
+public class BooleanVariable {
+    public static void main(String[] args) {
+        boolean isItRaining=false;
+        boolean areYouHungry=true;
+        System.out.println(isItRaining);
+        System.out.println(areYouHungry);
+    }
+}
